a46b006a66e1d0c8619dae10b88be2fe0cd5111e
ANZY KENYA <mugokevin900@gmail.com>
2022-12-25 21:40:09 +0300

    #2

--- src/Meme.js
+++ src/Meme.js
@@ -1,15 +1,36 @@
 import React from "react";
+import data from "./data";
 
-export default function meme(){
+export default function Meme(){
+    const [memeImage , setMemeImage] = React.useState("./images/meme.png");
+   let url ;
+    function getMemeImage(){
+        const memesArray = data.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
     return (
         <div className="main">
+                     <p>{url}</p>
+
             <form className="form">
                 <div className="inputs">
-<input className="form-inputs" type="text" placeholder="top text" ></input>
-<input className = "form-inputs" type="text" placeholder="bottom text"></input>
+    <input 
+    className="form-inputs" 
+    type="text"
+    placeholder="top text" />
+
+    <input 
+    className = "form-inputs" 
+    type="text" 
+    placeholder="bottom text" />
                 </div>
-                <button className="meme--button">Get a new meme image</button>
-                <img src={"./images/meme.png"} alt="" height="300px" width="500px"/>
+                <button className="meme--button"
+                         onClick={getMemeImage} >
+                    Get a new meme image
+                    </button>
+
+                <img src={memeImage} alt="" height="300px" width="500px"/>
             </form>
             
         </div>
---
+++ src/data.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default{
+    
+    "data": {
+        "memes": [
+            {
+                "id": "61579",
+                "name": "One Does Not Simply",
+                "url": "https://t3.ftcdn.net/jpg/03/64/95/94/360_F_364959444_AFyG7DdoWZ41P4EhcN4i9rkyhALri7Qj.jpg",
+                "width": 568,
+                "height": 335,
+                "box_count": 2
+            },
+            {
+                "id": "101470",
+                "name": "Ancient Aliens",
+                "url": "https://static01.nyt.com/images/2021/04/30/multimedia/30xp-meme/29xp-meme-mediumSquareAt3X-v5.jpg",
+                "width": 500,
+                "height": 437,
+                "box_count": 3
+            }
+            // probably a lot more memes here..
+        ]
+    }
+}
